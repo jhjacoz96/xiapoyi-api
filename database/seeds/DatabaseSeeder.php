9c6bf5424ecb3df4f134760341bca150cb16eb9f
Jhon Contreras <jhjacoz96@gmail.com>
2021-06-07 08:19:46 -0430

    fix seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GenderSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
-        $this->call(UserSeeder::class);
         $this->call(RoleHasPermissionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(RiskSeed::class);
