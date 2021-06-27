9671a5c5b9671d856ceecc6263b336f2babe4e6c
jhjacoz96 <jhjacoz96@gmail.com>
2021-06-27 09:30:58 -0430

    add download report file family

--- app/Http/Resources/ReportFileFamilyResource.php
+++ app/Http/Resources/ReportFileFamilyResource.php
@@ -16,7 +16,7 @@ class ReportFileFamilyResource extends JsonResource
     public function toArray($request)
     {
         $jefe_familia = Member::whereHas('relationship', function($query) {
-            $query->find(3);
+            $query->find(1);
         })->first();
        return [
             "id" => $this->id,
