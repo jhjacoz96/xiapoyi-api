27d3b9c4a5ab88b528cac784e251fba09cb1b722
jhjacoz96 <jhjacoz96@gmail.com>
2021-08-01 17:58:57 -0430

    fix table older adult

---
+++ database/migrations/2021_08_01_222752_fix_field_older_adults_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixFieldOlderAdultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('older_adults', function (Blueprint $table) {
+            $table->integer("show")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
