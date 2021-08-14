badd1004be284292f67c80dc0413bcc6906dbf5e
jhjacoz96 <jhjacoz96@gmail.com>
2021-08-13 23:59:07 -0430

    add task programimg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:clean')->dailyAt('02:00');
-        $schedule->command('backup:run --only-db')->dailyAt('03:00');
+        $schedule->command('backup:clean')->everyThreeMinutes();
+        $schedule->command('backup:run --only-db')->everyMinute();
     }
 
     /**
